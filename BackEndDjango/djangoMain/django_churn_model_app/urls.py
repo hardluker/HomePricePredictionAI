175975e584c887c96c83a19ae2138a4075f08776
hardluker <dba9514@gmail.com>
2024-05-19 07:08:38 -0400

    Commented out the train url to prevent training in production

--- BackEndDjango/djangoMain/django_churn_model_app/urls.py
+++ BackEndDjango/djangoMain/django_churn_model_app/urls.py
@@ -3,7 +3,7 @@ from .views import train_model, predict_home_value, predict_growth
 
 print("Loading URL patterns from APP")
 urlpatterns = [
-    path('train/', train_model, name='train_model'),
+    # path('train/', train_model, name='train_model'),
     path('predict/', predict_home_value, name='predict_home_value'),
     path('growth/', predict_growth, name='predict_growth'),
 ]
