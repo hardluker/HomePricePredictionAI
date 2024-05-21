ee402b02f6a2021483241be1e05e41bfb1aae095
hardluker <dba9514@gmail.com>
2024-05-21 19:07:41 -0400

    Building a form component

--- FrontEndReact/src/App.tsx
+++ FrontEndReact/src/App.tsx
@@ -1,20 +1,15 @@
 import { useState } from 'react';
 import './App.css';
+import { ExpandableText } from './components/ExpandableText';
 import produce from 'immer';
+import { Form } from './components/Form';
 
 function App() {
-  const [pizza, setPizza] = useState({
-    name: 'Spicy Pepperoni',
-    toppings: ['Mushroom']
-  });
-
-  const handleClick = () => {
-    setPizza({ ...pizza, toppings: [...pizza.toppings, 'Cheese'] });
-  };
   return (
     <>
-      <div>{pizza.toppings}</div>
-      <button onClick={handleClick}>Press Me</button>
+      <div>
+        <Form></Form>
+      </div>
     </>
   );
 }
