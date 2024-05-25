44a3506ccf544ad8a6fea812ab39e1a340fd125c
hardluker <dba9514@gmail.com>
2024-05-25 08:22:31 -0400

    Working with HTTP Requests

--- FrontEndReact/src/App.tsx
+++ FrontEndReact/src/App.tsx
@@ -1,23 +1,46 @@
 import './App.css';
 import { ProductList } from './components/ProductList';
 import { ExpenseTracker } from './expense-tracker/ExpenseTracker';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import axios, { CanceledError } from 'axios';
 
+interface User {
+  id: number;
+  name: string;
+}
 function App() {
-  const ref = useRef<HTMLInputElement>(null);
-
-  //Performs after rendering
-  useEffect(() => {});
+  const [users, setUsers] = useState<User[]>([]);
+  const [error, setError] = useState('');
+  const [isLoading, setLoading] = useState(false);
+  useEffect(() => {
+    const controller = new AbortController();
+    setLoading(true);
+    axios
+      .get<User[]>('https://jsonplaceholder.typicode.com/users', {
+        signal: controller.signal
+      })
+      .then((response) => {
+        setUsers(response.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+        setLoading(false);
+      });
+    return () => controller.abort();
+  }, []);
 
   return (
-    <div>
-      <select className="form-select">
-        <option value=""></option>
-        <option value="Clothing">Clothing</option>
-        <option value="Household">Household</option>
-      </select>
-      <ProductList />
-    </div>
+    <>
+      {error && <p className="text-danger">{error}</p>}
+      {isLoading && <div className="spinner-border"></div>}
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </>
   );
 }
 
--- FrontEndReact/src/components/ProductList.tsx
+++ FrontEndReact/src/components/ProductList.tsx
@@ -1,12 +1,12 @@
 import { useEffect, useState } from 'react';
 
-export const ProductList = () => {
+export const ProductList = ({ category }: { category: string }) => {
   const [products, setProducts] = useState<string[]>([]);
 
   useEffect(() => {
-    console.log('Fetching products');
+    console.log('Fetching products in', category);
     setProducts(['Clothing', 'Household']);
-  }, []);
+  }, [category]);
 
   return <div>ProductList</div>;
 };
